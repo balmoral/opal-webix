fb251b8fd1f234be9fc0550fe63db86b6dee94c7
balmoral <colgunn@me.com>
2015-08-18 15:10:32 +1000

    initial commit

--- lib/opal/webix/setup.rb
+++ lib/opal/webix/setup.rb
@@ -4,10 +4,16 @@ require 'native'
 # These requires must be loaded in order of dependency:
 require 'opal/webix/native/native_base'
 require 'opal/webix/ui'
+require 'opal/webix/ui/base/atom_render'
+require 'opal/webix/ui/base/base_bind'
+require 'opal/webix/ui/base/base_layout'
 require 'opal/webix/ui/base/base_view'
-require 'opal/webix/ui/view'
-require 'opal/webix/ui/button'
-
+require 'opal/webix/ui/base/destruction'
+require 'opal/webix/ui/base/event_system'
+require 'opal/webix/ui/base/settings'
+require 'opal/webix/ui/layout/layout'
+require 'opal/webix/ui/views/view'
+require 'opal/webix/ui/views/button'
 
 module Webix
 
