6c32790445dec7afb9cfee5184a1657374a448bd
balmoral <colgunn@me.com>
2015-08-19 09:40:56 +1000

    initial commit

--- lib/opal/webix/ui/ui.rb
+++ lib/opal/webix/ui/ui.rb
@@ -1,6 +1,8 @@
 # @see http://docs.webix.com/
 module Webix
   module UI
+    module_function
+
     def create(options)
       h = options.to_h
       console.log "#{self.class.name}##{__method__}[#{__LINE__}] : calling webix.ui(#{h})"
