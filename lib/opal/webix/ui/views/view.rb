30130ba8eaadecbee0bfc387c6f8bb3818b9a861
balmoral <colgunn@me.com>
2015-08-19 10:59:42 +1000

    initial commit

--- lib/opal/webix/ui/views/view.rb
+++ lib/opal/webix/ui/views/view.rb
@@ -6,5 +6,13 @@ module Webix
   # @see http://docs.webix.com/api__refs__ui.view.html
   class View
     include BaseView
+
+    alias_native :attach_event, :attachEvent
+    alias_native :block_event, :blockEvent
+    alias_native :call_event, :callEvent
+    alias_native :detach_event, :detachEvent
+    alias_native :has_event, :hasEvent
+    alias_native :map_event, :mapEvent
+    alias_native :unblock_event, :unblockEvent
   end
 end
