932f6483113576084d8f593f2eafb25e0f760c0b
balmoral <colgunn@me.com>
2015-08-20 16:45:09 +1000

    initial commit

--- lib/opal/webix/setup.rb
+++ lib/opal/webix/setup.rb
@@ -3,6 +3,7 @@ require 'native'
 
 # These requires must be loaded in order of dependency:
 require 'opal/webix/native/native_base'
+require 'opal/webix/helpers/common'
 require 'opal/webix/ui/ui'
 require 'opal/webix/ui/base/base_view'
 require 'opal/webix/ui/base/base_layout'
