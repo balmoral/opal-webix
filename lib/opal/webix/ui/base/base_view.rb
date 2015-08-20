c071e1dcd3c1b0254700a1aa1e1aefd9dabf2194
balmoral <colgunn@me.com>
2015-08-20 11:40:25 +1000

    initial commit

--- lib/opal/webix/ui/base/base_view.rb
+++ lib/opal/webix/ui/base/base_view.rb
@@ -83,10 +83,10 @@ module Webix
     def attach_event(event, proc = nil, &block)
       if proc
         puts "#{self.class.name}##{__method__}[#{__LINE__}](#{event}) : proc : call native attachEvent(#{event}, #{proc})"
-        debug_call(to_n, 'fsdffdsaffasf', event, proc)
+        debug_call(to_n, 'attachEvent', event, proc)
       elsif block
         puts "#{self.class.name}##{__method__}[#{__LINE__}](#{event}) : block : call native attachEvent(#{event}, #{block})"
-        debug_call(to_n, 'aasdfsdafsadfd', event, &block)
+        debug_call(to_n, 'attachEvent', event, &block)
       else
         raise ArgumentError, 'missing proc or block'
       end
