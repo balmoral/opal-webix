00b49661d30cf0f39d030d2f1e2ab4a99129c0b9
balmoral <colgunn@me.com>
2015-08-28 13:08:56 +1000

    initial commit

--- lib/opal/webix/setup.rb
+++ lib/opal/webix/setup.rb
@@ -6,12 +6,7 @@ require 'opal/webix/native/native_base'
 require 'opal/webix/native/methods_bridge'
 require 'opal/webix/helpers/common'
 require 'opal/webix/ui/ui'
-require 'opal/webix/ui/base/base_view'
-require 'opal/webix/ui/base/base_layout'
-require 'opal/webix/ui/base/proto'
-require 'opal/webix/ui/layout/layout'
-require 'opal/webix/ui/views/view'
-require 'opal/webix/ui/views/button'
+require 'opal/webix/ui/component'
 
 
 
