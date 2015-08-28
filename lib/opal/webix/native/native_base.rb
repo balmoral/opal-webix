78b7e6f26d66adf8ba32aa473279501015d33029
balmoral <colgunn@me.com>
2015-08-28 12:09:55 +1000

    initial commit

--- lib/opal/webix/native/native_base.rb
+++ lib/opal/webix/native/native_base.rb
@@ -1,56 +1,54 @@
-module Opal
-  module Webix
+module Opal; module Webix
 
+module NativePatches
+  extend Native::Helpers
 
-    module NativePatches
-      extend Native::Helpers
-
-      # Patch of Native.alias_native to provide us
-      # with ability to specify:
-      #    alias_native :ruby_name, :js_name, as_array_of: Class
-      # which will map the elements of the native array
-      # to elements type Class.
-      def alias_native(new, old = new, options = {})
-        if old.end_with? ?=
-          define_method new do |value|
-            `#@native[#{old[0 .. -2]}] = #{Native.convert(value)}`
-            value
+  # Patch of Native.alias_native to provide us
+  # with ability to specify:
+  #    alias_native :ruby_name, :js_name, as_array_of: Class
+  # which will map the elements of the native array
+  # to elements type Class.
+  def alias_native(new, old = new, options = {})
+    if old.end_with? ?=
+      define_method new do |value|
+        `#@native[#{old[0 .. -2]}] = #{Native.convert(value)}`
+        value
+      end
+    elsif klass = options[:as_array_of]
+      define_method new do |*args, &block|
+        if value = Native.call(@native, old, *args, &block)
+          value.map{ |e| klass.new(e) }
+        end
+      end
+    else
+      if as = options[:as]
+        define_method new do |*args, &block|
+          if value = Native.call(@native, old, *args, &block)
+            as.new(value.to_n)
           end
-        elsif klass = options[:as_array_of]
-          define_method new do |*args, &block|
-            if value = Native.call(@native, old, *args, &block)
-              value.map{ |e| klass.new(e) }
-            end
-          end
-        else
-          if as = options[:as]
-            define_method new do |*args, &block|
-              if value = Native.call(@native, old, *args, &block)
-                as.new(value.to_n)
-              end
-            end
-          else
-            define_method new do |*args, &block|
-              Native.call(@native, old, *args, &block)
-            end
-          end
+        end
+      else
+        define_method new do |*args, &block|
+          Native.call(@native, old, *args, &block)
         end
       end
     end
-
-    module NativeBase
-      include Native
-
-      def camel_case(string)
-        string.to_s.sub(/^[a-z]/){|a|a.upcase}.gsub(/[_\-][a-z]/) { |a| a[1].upcase }
-      end
-
-      def self.included(klass)
-        #klass.extend Native::Helpers
-        klass.extend NativePatches
-      end
-
-    end
   end
 end
+
+module NativeBase
+  include Native
+
+  def camel_case(string)
+    string.to_s.sub(/^[a-z]/){|a|a.upcase}.gsub(/[_\-][a-z]/) { |a| a[1].upcase }
+  end
+
+  def self.included(klass)
+    #klass.extend Native::Helpers
+    klass.extend NativePatches
+  end
+
+end
+
+end end
 
