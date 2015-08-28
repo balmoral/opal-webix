75c3d1cc475eb6c14da9cb78e81945ccf38c6d30
balmoral <colgunn@me.com>
2015-08-28 17:20:29 +1000

    initial commit

--- lib/opal/webix/ui/ui.rb
+++ lib/opal/webix/ui/ui.rb
@@ -1,45 +1,40 @@
 # @see http://docs.webix.com/
-module Opal
-  module Webix
+module Opal; module Webix
+  module_function
+
+  module UI
     module_function
 
-    module UI
-      module_function
-
-      # See Webix API docs for config options
-      # Caller should ensure the doc is ready
-      def <<(options)
-        h = strip_ids_from_ons(options.to_h)
-        # `console.log(#{"#{self.class.name}##{__method__}[#{__LINE__}] : calling webix.ui(#{h})"})`
-        `webix.ui(#{h.to_n})`
-        self
-      end
-      alias_method :add, :<<
-
-      # Returns an Opal::Webix::Component wrapper
-      # of Webix component with the given id.
-      # Returns nil if no component is found.
-      # Any Webix function relevant to the component
-      # may be called from Ruby. At present any
-      # value returned from these methods will be
-      # native objects, unless the value is a basic
-      # type: boolean, numeric, strings, etc.
-      # As appropriate you can wrap these values
-      # using Opal::Webix::Component.new(native)
-      # or Native.new(native).
-      def [](id)
-        o = `webix.$$(#{id})`
-        is_null = `o == null`
-        `console.log(#{"#{self.name}##{__method__}[#{__LINE__}](#{id}) o => #{o} is_null=#{is_null}"})`
-        is_null ? nil : Opal::Webix::Component.new(o)
-      end
-      alias_method :get, :[]
-
+    # See Webix API docs for config options
+    # Caller should ensure the doc is ready
+    def <<(options)
+      h = strip_ids_from_ons(options.to_h)
+      # `console.log(#{"#{self.class.name}##{__method__}[#{__LINE__}] : calling webix.ui(#{h})"})`
+      `webix.ui(#{h.to_n})`
+      self
     end
+    alias_method :add, :<<
 
-    def ui
-      UI
+    # Returns an Opal::Webix::Component wrapper
+    # of Webix component with the given id.
+    # Returns nil if no component is found.
+    # Any Webix function relevant to the component
+    # may be called from Ruby. At present any
+    # value returned from these methods will be
+    # native objects, unless the value is a basic
+    # type: boolean, numeric, strings, etc.
+    # As appropriate you can wrap these values
+    # using Opal::Webix::Component.new(native)
+    # or Native.new(native).
+    def [](id)
+      o = `webix.$$(#{id})`
+      is_null = `o == null`
+      `console.log(#{"#{self.name}##{__method__}[#{__LINE__}](#{id}) o => #{o} is_null=#{is_null}"})`
+      is_null ? nil : Opal::Webix::Component.new(o)
     end
+    alias_method :get, :[]
+
+    private
 
     # Volt::Model's add id's to all embedded models.
     # And Webix doesn't handle id's in 'on' hashes.
@@ -88,6 +83,12 @@ module Opal
       Volt.logger.debug "#{self.class.name}##{_method}[#{line}] : #{s}"
     end
 
+
   end
-end
+
+  def ui
+    UI
+  end
+
+end end
 
